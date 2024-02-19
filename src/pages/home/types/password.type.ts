c7f1503c977b67675adedb05f0d7efa6915aa78f
Nazar <nazaranddobriy@gmail.com>
2024-02-19 10:53:21 +0200

    feat: declare password types

---
+++ src/pages/home/types/password.type.ts
@@ -0,0 +1,13 @@
+export interface CharacterProperties {
+  isNumber: boolean;
+  isLetter: boolean;
+  isSymbol: boolean;
+};
+
+export enum QualityLevel {
+  Empty,
+  Invalid,
+  Simple,
+  Average,
+  Stable
+};
