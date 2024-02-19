fe62957a59e9c79f92a3e0b81fc36f2a93c4a2d2
Nazar <nazaranddobriy@gmail.com>
2024-02-19 10:56:54 +0200

    feat: add imports to home module

--- src/pages/home/home.module.ts
+++ src/pages/home/home.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { HomeComponent } from './home.component';
+import { PasswordInputComponent } from './components/password-input/password-input.component';
+import { PasswordService } from './providers/password.service';
+import { PasswordStatusFieldComponent } from './components/password-input/password-status-field/password-status-field.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent }
 ];
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, PasswordInputComponent, PasswordStatusFieldComponent],
   imports: [
     CommonModule,
+    FormsModule,
     RouterModule.forChild(routes)
-  ]
+  ],
+  providers: [PasswordService]
 })
 export class HomeModule { }
